0efcb201721399c794983270e799d4169e07ae8c
RamazanImashov <ramazanimashov.dev@gmail.com>
2024-03-12 16:10:33 +0600

    edit

---
+++ worker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+until cd /app/backend
+do
+    echo "Waiting for server volume..."
+done
+
+# run a worker :)
+celery -A backend worker --loglevel=info --concurrency 1 -E
+#!/bin/sh
+
+until cd /app/backend
+do
+    echo "Waiting for server volume..."
+done
+
+# run a worker :)
+celery -A backend worker --loglevel=info --concurrency 1 -E
