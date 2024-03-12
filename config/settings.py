1c371c8a05d5fc4eb26c8d97b453ee8fcaa7c492
RamazanImashov <ramazanimashov.dev@gmail.com>
2024-03-12 16:25:02 +0600

    postgers

--- config/settings.py
+++ config/settings.py
@@ -94,13 +94,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config('DB_NAME'),
+        'USER': config('DB_USER'),
+        "PASSWORD": config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
